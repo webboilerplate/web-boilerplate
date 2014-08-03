d27545b5f727b4873346597c7f7903e1fff3c401
Sönke Kluth <soenke.kluth@gmail.com>
2014-08-03 21:19:49 +0200

    update gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,16 +6,20 @@
 
 var gulp = require('gulp');
 var $ = require('gulp-load-plugins')();
-var runSequence = require('run-sequence');
+
 var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
 var ip = require('ip');
+var runSequence = require('run-sequence');
 var pagespeed = require('psi');
-var handleErrors = require('./gulp/util/handleErrors');
 var fs = require('fs');
 var rsync = require('rsyncwrapper').rsync;
 var pkg = require('./package.json');
-var reload = browserSync.reload;
 
+var spritesmith = require('gulp.spritesmith');
+
+var path = require('path');
 var config = require('./config');
 var folders = config.folders;
 
@@ -32,6 +36,20 @@ var banner = ['/**',
     Helper
 *******************************************************************************/
 
+var handleErrors = function() {
+  var args = Array.prototype.slice.call(arguments);
+
+  // Send error to notification center with gulp-notify
+  $.notify.onError({
+    title: 'Compile Error',
+    message: '<%= error.message %>'
+  }).apply(this, args);
+
+  // Keep gulp from hanging on this task
+  this.emit('end');
+};
+
+
 /*******************************************************************************
     FILE / PATH / SHIM  CONFIG
 *******************************************************************************/
@@ -42,8 +60,11 @@ var banner = ['/**',
 *******************************************************************************/
 gulp.task('clean', function() {
   return gulp.src([
-      folders.dest + '/assets/css', folders.dest + '/assets/js',
-      folders.dest + '/assets/fonts', folders.dest + '/assets/images'
+      folders.destAssets
+      // path.join(folders.destAssets, config.styles.css.folder),
+      // path.join(folders.destAssets, config.js.folder),
+      // path.join(folders.destAssets, config.fonts.folder),
+      // path.join(folders.destAssets, config.images.folder)
     ], {
       read: false
     })
@@ -56,70 +77,61 @@ gulp.task('clean', function() {
 *******************************************************************************/
 
 
-
 gulp.task('scss', function() {
-  return gulp.src(folders.src + '/assets/scss/main.scss')
+  return gulp.src(folders.srcAssets + '/' + config.styles.scss.folder + '/' + config.styles.scss.main)
     .pipe($.plumber())
     .pipe($.sass({
-      includePaths: folders.sassIncludePaths,
+      includePaths: folders.components,
       outputStyle: 'expanded'
     }))
     .on('error', handleErrors)
     .pipe($.autoprefixer(config.autoprefixer.def))
-    .pipe(gulp.dest(folders.tmp + '/assets/css'));
+    .pipe(gulp.dest(folders.tmpAssets + '/css'))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 
 gulp.task('compass', function() {
-  return gulp.src(folders.src + '/assets/scss/*.{scss,sass}')
+  return gulp.src(folders.srcAssets + '/' + config.styles.scss.folder + '/' + config.styles.scss.main)
     .pipe($.plumber())
     .pipe($.compass({
-      // css: folders.src + '/assets/css',
-      css: folders.tmp + '/assets/css',
-      sass: folders.src + '/assets/scss',
-      image: folders.src + '/assets/images',
-      javascripts: folders.src + '/assets/js',
-      fonts: folders.src + '/assets/fonts',
-      import_path: folders.componentsPath
-        /*,
-        require: ['susy', 'modular-scale']*/
+      css: folders.tmpAssets + '/' + config.styles.css.folder,
+      sass: folders.srcAssets + '/' + config.styles.scss.folder,
+      image: folders.srcAssets + '/' + config.images.folder,
+      javascripts: folders.srcAssets + '/' + config.js.folder,
+      fonts: folders.srcAssets + '/' + config.fonts.folder,
+      import_path: folders.components
+      //, require: ['susy', 'modular-scale']
     }))
     .on('error', handleErrors)
     .pipe($.autoprefixer(config.autoprefixer.def))
-    .pipe(gulp.dest(folders.tmp + '/assets/css'));
+    .pipe(gulp.dest(folders.tmpAssets + '/css'))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 
-
 gulp.task('stylus', function() {
-  return gulp.src(folders.src + '/assets/stylus/main.styl')
+  return gulp.src(folders.srcAssets + '/' + config.styles.stylus.folder + '/' + config.styles.stylus.main)
     .pipe($.plumber())
     .pipe($.stylus({
       //use: nib(),
       url: {
         name: 'embedurl',
-        paths: [__dirname + '/src/assets/images'],
+        paths: [__dirname + '/' + folders.srcAssets + '/' + config.images.folder],
         limit: false
       },
       error: true
     }))
     .on('error', handleErrors)
     .pipe($.autoprefixer.apply(config.autoprefixer.def))
-    .pipe(gulp.dest(folders.tmp + '/assets/css'));
-  // .pipe(browserSync.reload({
-  //     stream: true
-  // }));
-});
-
-
-
-
-// Automatically Prefix CSS
-gulp.task('autoprefixer', function() {
-  return gulp.src(folders.tmp + '/assets/css/**/*.css')
-    .pipe($.changed(folders.tmp + '/assets/css/'))
-    .pipe($.autoprefixer.apply(config.autoprefixer.def))
-    .pipe(gulp.dest(folders.tmp + '/assets/css'));
+    .pipe(gulp.dest(folders.tmpAssets + '/css'))
+    .pipe(reload({
+      stream: true
+    }));
 });
 
 
@@ -131,8 +143,7 @@ gulp.task('csslint', function() {
 
 
 gulp.task('styles', function(cb) {
-  return gulp.start(config.css.preprocessor);
-  // return runSequence('process-css', ['sass', 'compass'], 'stylus', cb);
+  return gulp.start(config.styles.preprocessor);
 });
 
 
@@ -148,7 +159,6 @@ gulp.task('jshint', function() {
 });
 
 
-
 gulp.task('js', function() {
   return gulp.src(folders.src + '/assets/js/main.js')
     .pipe($.plumber())
@@ -159,9 +169,6 @@ gulp.task('js', function() {
     }))
     .on('error', handleErrors)
     .pipe($.rename('app.js'))
-    .pipe($.header(banner, {
-      pkg: pkg
-    }))
     .pipe(gulp.dest(folders.tmp + '/assets/js'))
     .pipe(reload({
       stream: true,
@@ -170,15 +177,14 @@ gulp.task('js', function() {
 });
 
 
-gulp.task('modernizr', function() {
+gulp.task('vendor', function() {
   return gulp.src(folders.src + '/assets/js/vendor/*.js')
     .pipe(gulp.dest(folders.tmp + '/assets/js/vendor'));
 });
 
 
-
 gulp.task('scripts', function(cb) {
-  return runSequence('jshint', ['js', 'modernizr'], cb);
+  return runSequence('jshint', ['js', 'vendor'], cb);
 });
 
 
@@ -201,6 +207,19 @@ gulp.task('images', function() {
 });
 
 
+gulp.task('sprites', function() {
+  var spriteData = gulp.src(folders.src + '/assets/images/sprites/*.png')
+    .pipe(spritesmith({
+      imgName: 'sprite.png',
+      imgPath: '../images/sprite.png',
+      padding: 10,
+      cssName: 'sprites.scss'
+    }));
+
+  spriteData.img.pipe(gulp.dest(folders.src + '/assets/images/'));
+  spriteData.css.pipe(gulp.dest(folders.src + '/assets/scss/components/utils/'));
+});
+
 
 /*******************************************************************************
     TEMPLATE TASK
@@ -213,7 +232,7 @@ gulp.task('images', function() {
 
 
 /*******************************************************************************
-    BUILD TASK
+    BUILD / DIST TASK
 *******************************************************************************/
 
 
@@ -255,11 +274,6 @@ gulp.task('bump:major', function() {
 
 
 
-
-
-//DIST
-
-
 //TODO banner gets written in every vendor.
 gulp.task('js:dist', function() {
   return gulp.src(folders.tmp + '/assets/js/**/*.js')
@@ -269,7 +283,6 @@ gulp.task('js:dist', function() {
     }))
     .pipe(gulp.dest(folders.dest + '/assets/js'));
 });
-
 
 gulp.task('css:dist', function() {
   return gulp.src(folders.tmp + '/assets/css/**/*.css')
@@ -285,10 +298,9 @@ gulp.task('fonts:dist', function() {
     .pipe(gulp.dest(folders.dest + '/assets/fonts'));
 });
 
-//TODO vereinfachen:
-gulp.task('html:root:dist', function() {
-  return gulp.src([folders.src + '/*.*', '!' + folders.bower + '/**/*'])
-    .pipe(gulp.dest(folders.dest));
+gulp.task('images:dist', function() {
+  return gulp.src([folders.src + '/assets/images/**/*', '!' + folders.src + '/assets/images/sprites'])
+    .pipe(gulp.dest(folders.dest + '/assets/images'));
 });
 
 gulp.task('html:tmp:dist', function() {
@@ -297,16 +309,13 @@ gulp.task('html:tmp:dist', function() {
 });
 
 gulp.task('html:src:dist', function() {
-  return gulp.src([folders.src + '/**/*.{html,shtml,php,xml,json}', '!' + folders.bower + '/**/*'])
+  return gulp.src([folders.src + '/*.*', folders.src + '/**/*.{html,shtml,php,xml,json}', '!' + folders.bower + '/**/*', '!' + folders.srcAssets + '/js/**/*'])
     .pipe(gulp.dest(folders.dest));
 });
 
-
-
 gulp.task('dist', function(cb) {
-  runSequence('clean', 'default', ['js:dist', 'css:dist', 'fonts:dist', 'html:root:dist', 'html:tmp:dist', 'html:src:dist'], cb);
+  runSequence('clean', 'default', ['js:dist', 'css:dist', 'fonts:dist', 'images:dist'], 'html:src:dist', 'html:tmp:dist', cb);
 });
-
 
 gulp.task('dist:bump', ['bump:patch', 'dist']);
 
@@ -316,7 +325,7 @@ gulp.task('dist:bump', ['bump:patch', 'dist']);
     DEPLOY TASK
 *******************************************************************************/
 
-gulp.task('deploy', ['dist:bump'], function(cb) {
+gulp.task('deploy', ['dist'], function(cb) {
   rsync({
     args: ['--verbose'],
     src: './dist/',
@@ -340,16 +349,6 @@ gulp.task('deploy', ['dist:bump'], function(cb) {
 
 
 
-// Build and serve the output from the dist build
-gulp.task('serve:dist', ['dist'], function() {
-  browserSync({
-    notify: false,
-    server: {
-      baseDir: folders.dest
-    }
-  });
-});
-
 /*******************************************************************************
     PageSpeed TASK
 *******************************************************************************/
@@ -369,13 +368,28 @@ gulp.task('pagespeed', pagespeed.bind(null, {
 
 
 /*******************************************************************************
-    WATCH TASK
+    WATCH / SERVE TASK
 *******************************************************************************/
+
+// Build and serve the output from the dist build
+gulp.task('serve:dist', ['dist'], function() {
+  browserSync({
+    notify: false,
+    server: {
+      baseDir: folders.dest
+    }
+  });
+});
+
+
 
 gulp.task('watch', function() {
 
+  var openBrowserWindow = !($.util.env.restart || false);
+
   browserSync({
     notify: false,
+    open: openBrowserWindow,
     server: {
       baseDir: ['.tmp', 'src']
     }
@@ -385,10 +399,10 @@ gulp.task('watch', function() {
   gulp.watch([folders.src + '/**/*.{html,shtml,php,xml,json}'], reload);
 
   // Watch .scss files
-  gulp.watch(folders.src + '/assets/scss/**/*.scss', ['scss', reload]);
+  gulp.watch(folders.src + '/assets/scss/**/*.scss', ['scss']);
 
   // Watch .stylus files
-  gulp.watch(folders.src + '/assets/stylus/**/*.styl', ['stylus', reload]);
+  gulp.watch(folders.src + '/assets/stylus/**/*.styl', ['stylus']);
 
   // Watch .css files
   //gulp.watch(['{.tmp,app}/assets/css/**/*.css'], ['autoprefixer', reload]);
@@ -406,5 +420,7 @@ gulp.task('watch', function() {
 
 
 gulp.task('default', function() {
-  gulp.start('scripts', 'styles', 'images');
+  gulp.start('scripts', 'images', 'styles');
 });
+
+gulp.task('dev', ['default', 'watch']);
