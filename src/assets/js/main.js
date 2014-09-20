fdeea553acf651796102962090ae4eab3b26df8e
Sönke Kluth <soenke.kluth@gmail.com>
2014-09-20 21:36:55 +0200

    clean code

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,31 +1,25 @@
 'use strict';
 
-require('./app/util/error')();
-
 var domReady = require('./lib/domready');
 var fastdom = require('fastdom');
 var FastClick = require('fastclick');
 var polyfills = require('./polyfills');
 
-//TODO implement
-// var Ajaxify = require('./app/util/ajaxify/');
-// new Ajaxify();
-
 var app = require('./app');
-
-
 console.log('Moin Moin and welcome to ' + app.name);
 
-//TODO wech damit.
-var page = document.body.getElementsByClassName('page')[0];
-page.classList.add('page-current');
 
+var startTimeOut = 0;
 
 var onLoad = function() {
 
+  if (startTimeOut) {
+    clearTimeout(startTimeOut);
+  }
+
   app.start();
 
-  var fastClick = new FastClick(document.body,{});
+  var fastClick = new FastClick(document.body, {});
 
   fastdom.write(function() {
     document.body.classList.add('loaded');
@@ -33,9 +27,9 @@ var onLoad = function() {
 
 };
 
-
 if (document.readyState === 'complete') {
   domReady(onLoad);
 } else {
+  startTimeOut = setTimeout(onLoad, 2600);
   window.onload = onLoad;
 }
