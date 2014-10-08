e52f0f417b53c49c06ffc94bdc7ce2149daad2df
Sönke Kluth <soenke.kluth@gmail.com>
2014-10-08 18:52:57 +0200

    add info to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
-'use strict';
+/*
+  gulpfile.js
+  ===========
+  Rather than manage one giant configuration file responsible
+  for creating multiple tasks, each task has been broken out into
+  its own file in gulp/tasks. Any files in that directory get
+  automatically required below.
 
+  To add a new task, simply add a new task file that directory.
+  gulp/tasks/default.js specifies the default set of tasks to run
+  when you run `gulp`.
+
+  all main tasks are listed here
+*/
+
+'use strict';
 
 var gulp = require('gulp');
 var runSequence = require('run-sequence');
-
 var config = require('./gulp/config');
 
-// Require all tasks in gulp/tasks, including subfolders
 require('require-dir')('./gulp/tasks', {
   recurse: true
 });
@@ -49,7 +61,7 @@ gulp.task('release', function(cb) {
 
 
 /*******************************************************************************
-    CONCRETE TASKS
+    TASKS
 *******************************************************************************/
 
 gulp.task('styles', function(cb) {
