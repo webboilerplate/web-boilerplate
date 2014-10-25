5c13bfc82977f78e767f1a60965dda0dd9cfc0cc
Sönke Kluth <soenke.kluth@gmail.com>
2014-10-25 21:36:49 +0200

    add interval to watch task

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,17 +2,21 @@
 
 var gulp = require('gulp');
 var paths = require('../config').paths;
-
+var reload = require('browser-sync').reload;
 var browserSync = require('browser-sync');
 
 /*******************************************************************************
     WATCH TASK
 *******************************************************************************/
 
-gulp.task('watch', function() {
+
+gulp.task('watch', [ /*'setWatching', 'browserify'*/ ], function() {
+
 
   // Watch for changes in `src` folder
-  gulp.watch(paths.src + '/**/*.{html,shtml,php,xml,json}', browserSync.reload);
+  gulp.watch(paths.src + '/**/*.{html,shtml,php,xml,json}', {
+    interval: 500
+  }, reload);
 
   // Watch .scss files
   gulp.watch(paths.src + '/' + paths.scss + '/**/*.scss', ['scss']);
@@ -21,18 +25,17 @@ gulp.task('watch', function() {
   gulp.watch(paths.src + '/' + paths.stylus + '/**/*.styl', ['stylus']);
 
   // Watch .js files
-  gulp.watch([paths.src + '/' + paths.js + '/**/*.js'], ['js']);
+  gulp.watch([paths.src + '/' + paths.js + '/**/*.js'], ['js', reload]);
 
   // Watch .jade files
   gulp.watch(paths.src + '/' + paths.jade + '/**/*.jade', ['jade']);
 
   // Watch sprite changes
-  gulp.watch(paths.src + '/' + paths.sprites + '/*.{png,jpg,gif}', ['sprites', browserSync.reload]);
+  gulp.watch(paths.src + '/' + paths.sprites + '/*.{png,jpg,gif}', {
+    interval: 1000
+  }, ['sprites', reload]);
 
   // Watch image files
   // gulp.watch(paths.src + '/assets/images/**/*', ['images']);
   //
-  // Watch sprite changes
-  gulp.watch(paths.src + '/' + paths.sprites + '/*.png', ['sprites']);
-
 });
